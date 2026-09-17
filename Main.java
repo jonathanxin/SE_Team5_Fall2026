a9a3da41e14b05dc08b59bdd408aa677adba2af9
Bradley Ising <bradleyvising@gmail.com>
2026-09-16 23:53:31 -0500

    setup JavaFX

--- Main.java
+++ Main.java
@@ -1,11 +1,31 @@
 // needs objects for model, view, controller
-public class Main {
+
+import java.util.Scanner;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
     Controller controller;
     Model model;    
-    View view;
     
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("View.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public static void main(String[] args) {
+        launch(args);
         System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+        System.out.println("You entered: " + number);
         // // main loop
         // do
         // {
