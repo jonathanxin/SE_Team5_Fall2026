4f3080c84b8249afcbfa59fc1e129a8d1f5057a3
SamekhKusma <lolurgent45@gmail.com>
2026-09-24 14:12:35 -0500

    Updated Model.java

--- Model.java
+++ Model.java
@@ -1,7 +1,17 @@
 // update all sprites
 public class Model {
+
+    private PlayerDatabase playerDatabase;
+
+    public Model() {
+        playerDatabase = new PlayerDatabase();
+    }
+
     public void update() {
         // Implement the logic to update the model state
     }
 
+    public PlayerDatabase getPlayerDatabase() {
+        return playerDatabase;
+    }
 }
